bedda4f05ebc5143ea078481ecd4d0b9da27c5f6
Joel Strand <jollestrand91@gmail.com>
2023-05-26 10:33:56 +0200

    Modded version for special case

--- vem_ar_vem.py
+++ vem_ar_vem.py
@@ -3,16 +3,29 @@ import random
 medlemmar = ["MangeMungo", "Borksson", "Flarmenko", "Viking Line", "C-mannen", "ShwindelyShwundely"]
 roller = ["Mamma", "Pappa", "Joel", "Märta", "Klara", "Farfar"]
 
-priser = ["Paketpris 1", "Paketpris 2", "Sista ner 1", "Sista ner 2", "Första hem 1", "Första hem 2"]
+# priser = ["Paketpris 1", "Paketpris 2", "Sista ner 1", "Sista ner 2", "Första hem 1", "Första hem 2"]
+priser = ["Första ner", "Andra ner", "Första hem", "Andra hem"]
 
 random.shuffle(medlemmar)
 
+redteam = ["Tom", "Tom"]
+goldteam = []
+
 for i in range(len(medlemmar)):
     print(roller[i] + " spelas dennan resa av " + medlemmar[i])
+    # Special hack for special people
+    if medlemmar[i] == "MangeMungo":
+        redteam[0] = roller[i]
+    elif medlemmar[i] == "ShwindelyShwundely":
+        redteam[1] = roller[i]
+    else:
+        goldteam.append(roller[i])
 
 print()
 
-random.shuffle(roller)
-
-for i in range(len(medlemmar)):
-    print(priser[i] + " vanns denna gången av " + roller[i])
+# random.shuffle(roller)
+random.shuffle(goldteam)
+print("Paketpris vanns denna gången av " + redteam[0])
+print("Sista ner vanns denna gången av " + redteam[1])
+for i in range(len(goldteam)):
+    print(priser[i] + " vanns denna gången av " + goldteam[i])
