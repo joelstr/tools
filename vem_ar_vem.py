7c0620d612e83b7888618156f72a7b3e52abf351
Joel Strand <jollestrand91@gmail.com>
2023-05-26 10:23:00 +0200

    Adding default file

--- vem_ar_vem.py
+++ vem_ar_vem.py
@@ -1,5 +1,18 @@
-medlemmar = ["MangeMungo", "Borksson", "Flarmenko", "Viking Line", "C-mannen","ShwindelyShwundely"]
+import random
+
+medlemmar = ["MangeMungo", "Borksson", "Flarmenko", "Viking Line", "C-mannen", "ShwindelyShwundely"]
 roller = ["Mamma", "Pappa", "Joel", "Märta", "Klara", "Farfar"]
+
+priser = ["Paketpris 1", "Paketpris 2", "Sista ner 1", "Sista ner 2", "Första hem 1", "Första hem 2"]
+
+random.shuffle(medlemmar)
 
 for i in range(len(medlemmar)):
     print(roller[i] + " spelas dennan resa av " + medlemmar[i])
+
+print()
+
+random.shuffle(roller)
+
+for i in range(len(medlemmar)):
+    print(priser[i] + " vanns denna gången av " + roller[i])
