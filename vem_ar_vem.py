8f5363b8150f2e9d2c6b609caae264d73b6139f1
Joel Strand <jollestrand91@gmail.com>
2023-05-26 10:01:28 +0200

    First test

---
+++ vem_ar_vem.py
@@ -0,0 +1,5 @@
+medlemmar = ["MangeMungo", "Borksson", "Flarmenko", "Viking Line", "C-mannen","ShwindelyShwundely"]
+roller = ["Mamma", "Pappa", "Joel", "Märta", "Klara", "Farfar"]
+
+for i in range(len(medlemmar)):
+    print(roller[i] + " spelas dennan resa av " + medlemmar[i])
